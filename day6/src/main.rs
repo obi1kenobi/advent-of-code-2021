b60d308789d3dcccb1c9a7f1bc0f0595b843c1b7
Predrag Gruevski <obi1kenobi82@gmail.com>
2021-12-07 08:49:48 -0500

    Add day6 solution.

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -15,9 +15,9 @@ fn main() {
     let input_file = reversed_args.pop().expect("input file");
     let content = fs::read_to_string(input_file).unwrap();
 
-    let input_data: Vec<u64> = content
+    let input_data: Vec<usize> = content
         .trim_end()
-        .split('\n')
+        .split(',')
         .map(|x| x.parse().unwrap())
         .collect();
 
@@ -34,10 +34,32 @@ fn main() {
     }
 }
 
-fn solve_part1(data: &[u64]) -> usize {
-    todo!()
+fn simulate_fish(data: &[usize], total_days: usize) -> usize {
+    let mut today: [usize; 9] = Default::default();
+    let mut tomorrow: [usize; 9] = Default::default();
+
+    for fish in data {
+        tomorrow[*fish] += 1;
+    }
+
+    for _tomorrow_is_after_day in 1..=total_days {
+        // Tomorrow becomes today.
+        today[0..9].clone_from_slice(&tomorrow[0..9]);
+
+        // Advance the timers.
+        tomorrow[0..8].clone_from_slice(&today[1..9usize]);
+
+        tomorrow[8] = today[0];   // Spawn new fish, then
+        tomorrow[6] += today[0];  // add the spawning fish back to the queue.
+    }
+
+    tomorrow.iter().sum()
 }
 
-fn solve_part2(data: &[u64]) -> usize {
-    todo!()
+fn solve_part1(data: &[usize]) -> usize {
+    simulate_fish(data, 80)
+}
+
+fn solve_part2(data: &[usize]) -> usize {
+    simulate_fish(data, 256)
 }
