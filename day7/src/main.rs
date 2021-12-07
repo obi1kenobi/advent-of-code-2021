84ec1029e211c989f287855fab276b14d05912cc
Predrag Gruevski <obi1kenobi82@gmail.com>
2021-12-07 10:23:01 -0500

    Add solution for day7.

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -15,11 +15,12 @@ fn main() {
     let input_file = reversed_args.pop().expect("input file");
     let content = fs::read_to_string(input_file).unwrap();
 
-    let input_data: Vec<u64> = content
+    let mut input_data: Vec<u64> = content
         .trim_end()
-        .split('\n')
+        .split(',')
         .map(|x| x.parse().unwrap())
         .collect();
+    input_data.sort_unstable();
 
     match part {
         "1" => {
@@ -34,10 +35,64 @@ fn main() {
     }
 }
 
-fn solve_part1(data: &[u64]) -> usize {
-    todo!()
+fn solve_part1(positions: &[u64]) -> u64 {
+    let mut cost: u64 = positions.iter().map(|pos| *pos - positions[0]).sum();
+    let mut lowest_cost = cost;
+
+    let mut left_crabs = 0u64;
+    let mut right_crabs = positions.len() as u64;
+
+    for (previous_pos, current_pos) in positions.iter().tuple_windows() {
+        left_crabs += 1;
+        right_crabs -= 1;
+
+        assert!(left_crabs < positions.len() as u64);
+
+        let distance = *current_pos - *previous_pos;
+        cost += distance * left_crabs;
+        cost -= distance * right_crabs;
+
+        if cost < lowest_cost {
+            lowest_cost = cost;
+        }
+    }
+
+    lowest_cost
 }
 
-fn solve_part2(data: &[u64]) -> usize {
-    todo!()
+fn solve_part2(positions: &[u64]) -> u64 {
+    let mut cost: u64 = positions.iter().map(|pos| {
+        let delta = *pos - positions[0];
+        delta * (delta + 1) / 2
+    }).sum();
+    let mut lowest_cost = cost;
+
+    let mut left_crabs = 0u64;
+    let mut right_crabs = positions.len() as u64;
+
+    let mut delta_positive = 0u64;
+    let mut delta_negative: u64 = positions.iter().map(|pos| *pos - positions[0]).sum();
+
+    for (previous_pos, current_pos) in positions.iter().tuple_windows() {
+        left_crabs += 1;
+        right_crabs -= 1;
+
+        assert!(left_crabs < positions.len() as u64);
+
+        let distance = *current_pos - *previous_pos;
+        for _offset in 0..distance {
+            delta_positive += left_crabs;
+
+            cost += delta_positive;
+            cost -= delta_negative;
+
+            if cost < lowest_cost {
+                lowest_cost = cost;
+            }
+
+            delta_negative -= right_crabs;
+        }
+    }
+
+    lowest_cost
 }
