39d82fad1018878e603a1fe97422780fe6c788dd
Predrag Gruevski <obi1kenobi82@gmail.com>
2021-12-10 17:48:11 -0500

    Solve day10.

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -2,6 +2,7 @@ use std::{env, fs};
 
 #[allow(unused_imports)]
 use itertools::Itertools;
+use kth::SliceExtKth;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,11 +16,7 @@ fn main() {
     let input_file = reversed_args.pop().expect("input file");
     let content = fs::read_to_string(input_file).unwrap();
 
-    let input_data: Vec<u64> = content
-        .trim_end()
-        .split('\n')
-        .map(|x| x.parse().unwrap())
-        .collect();
+    let input_data: Vec<&str> = content.trim_end().split('\n').collect();
 
     match part {
         "1" => {
@@ -34,10 +31,94 @@ fn main() {
     }
 }
 
-fn solve_part1(data: &[u64]) -> usize {
-    todo!()
+fn matching_opening_char(c: char) -> char {
+    match c {
+        ')' => '(',
+        ']' => '[',
+        '}' => '{',
+        '>' => '<',
+        _ => unreachable!("{}", c),
+    }
 }
 
-fn solve_part2(data: &[u64]) -> usize {
-    todo!()
+fn matching_closing_char(c: char) -> char {
+    match c {
+        '(' => ')',
+        '[' => ']',
+        '{' => '}',
+        '<' => '>',
+        _ => unreachable!("{}", c),
+    }
+}
+
+// Result <
+//  remaining stack if any,
+//  (remaining stack, first illegal char, expected char or None for any open chunk char)
+// >
+fn check_content(content: &str) -> Result<Option<Vec<char>>, (Vec<char>, char, Option<char>)> {
+    let mut stack: Vec<char> = vec![];
+    for c in content.chars() {
+        match c {
+            '(' | '[' | '{' | '<' => stack.push(c),
+            ')' | ']' | '}' | '>' => {
+                let opening_char = matching_opening_char(c);
+                match stack.pop() {
+                    Some(open) if open == opening_char => {}
+                    Some(_) => {
+                        return Err((stack, c, Some(opening_char)));
+                    }
+                    None => {
+                        return Err((stack, c, None));
+                    }
+                }
+            }
+            _ => unreachable!("{}", c),
+        }
+    }
+
+    if stack.is_empty() {
+        Ok(None)
+    } else {
+        Ok(Some(stack))
+    }
+}
+
+fn solve_part1(data: &[&str]) -> u64 {
+    data.iter()
+        .map(|content| match check_content(content) {
+            Err((_, error_char, _)) => match error_char {
+                ')' => 3,
+                ']' => 57,
+                '}' => 1197,
+                '>' => 25137,
+                _ => unreachable!("{}", error_char),
+            },
+            Ok(_) => 0,
+        })
+        .sum()
+}
+
+fn solve_part2(data: &[&str]) -> u64 {
+    let mut scores: Vec<_> = data.iter().filter_map(|content| match check_content(content) {
+        Err(_) | Ok(None) => None,
+        Ok(Some(remaining_stack)) => {
+            let value = remaining_stack
+                .iter()
+                .rev()
+                .copied()
+                .map(|c| match matching_closing_char(c) {
+                    ')' => 1,
+                    ']' => 2,
+                    '}' => 3,
+                    '>' => 4,
+                    _ => unreachable!("{}", c),
+                })
+                .fold(0, |acc, x| acc * 5 + x);
+            Some(value)
+        }
+    }).collect();
+
+    let median_index = scores.len() / 2;
+    scores.partition_by_kth(median_index);
+    scores[median_index]
 }
