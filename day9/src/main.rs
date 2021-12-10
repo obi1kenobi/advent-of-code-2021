957bde948113e509782c46ff926d2c941bb204e8
Predrag Gruevski <obi1kenobi82@gmail.com>
2021-12-10 02:01:42 -0500

    Add stubs for days 9-11.

---
+++ day9/src/main.rs
@@ -0,0 +1,43 @@
+use std::{env, fs};
+
+#[allow(unused_imports)]
+use itertools::Itertools;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut reversed_args: Vec<_> = args.iter().map(|x| x.as_str()).rev().collect();
+
+    reversed_args
+        .pop()
+        .expect("Expected the executable name to be the first argument, but was missing");
+
+    let part = reversed_args.pop().expect("part number");
+    let input_file = reversed_args.pop().expect("input file");
+    let content = fs::read_to_string(input_file).unwrap();
+
+    let input_data: Vec<u64> = content
+        .trim_end()
+        .split('\n')
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    match part {
+        "1" => {
+            let result = solve_part1(&input_data);
+            println!("{}", result);
+        }
+        "2" => {
+            let result = solve_part2(&input_data);
+            println!("{}", result);
+        }
+        _ => unreachable!("{}", part),
+    }
+}
+
+fn solve_part1(data: &[u64]) -> usize {
+    todo!()
+}
+
+fn solve_part2(data: &[u64]) -> usize {
+    todo!()
+}
