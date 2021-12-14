7f14a0c0f0e233bbfc58eff06b3deff08557f589
Predrag Gruevski <obi1kenobi82@gmail.com>
2021-12-14 08:55:28 -0500

    Solve day14.

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs};
+use std::{collections::HashMap, env, fs};
 
 #[allow(unused_imports)]
 use itertools::Itertools;
@@ -15,31 +15,89 @@ fn main() {
     let input_file = reversed_args.pop().expect("input file");
     let content = fs::read_to_string(input_file).unwrap();
 
-    let input_data: Vec<u64> = content
-        .trim_end()
+    let (base, rules_component) = content.trim_end().split_once("\n\n").unwrap();
+
+    let rules: Vec<_> = rules_component
         .split('\n')
-        .map(|x| x.parse().unwrap())
+        .map(|row| {
+            let (from, to) = row.split_once(" -> ").unwrap();
+            assert_eq!(from.len(), 2);
+            assert_eq!(to.len(), 1);
+
+            let (from_first, from_second) = from.chars().collect_tuple().unwrap();
+            let inserted = to.chars().collect_tuple::<(char,)>().unwrap().0;
+
+            (from_first, from_second, inserted)
+        })
         .collect();
 
     match part {
         "1" => {
-            let result = solve_part1(&input_data);
+            let result = solve_part1(base, &rules);
             println!("{}", result);
         }
         "2" => {
-            let result = solve_part2(&input_data);
+            let result = solve_part2(base, &rules);
             println!("{}", result);
         }
         _ => unreachable!("{}", part),
     }
 }
 
-#[allow(unused_variables)]
-fn solve_part1(data: &[u64]) -> usize {
-    todo!()
+fn expand_polymer(
+    original: &HashMap<(char, char), usize>,
+    rules: &HashMap<(char, char), char>,
+) -> HashMap<(char, char), usize> {
+    let mut result = HashMap::with_capacity(original.len());
+
+    for (pair, count) in original {
+        if let Some(inserted) = rules.get(pair) {
+            let (first, second) = *pair;
+            *result.entry((first, *inserted)).or_default() += *count;
+            *result.entry((*inserted, second)).or_default() += *count;
+        } else {
+            *result.entry(*pair).or_default() += *count;
+        }
+    }
+
+    result
 }
 
-#[allow(unused_variables)]
-fn solve_part2(data: &[u64]) -> usize {
-    todo!()
+fn solve_iterations(original: &str, rules: &[(char, char, char)], iterations: usize) -> usize {
+    let rules: HashMap<(char, char), char> = rules
+        .iter()
+        .copied()
+        .map(|(a, b, insert)| ((a, b), insert))
+        .collect();
+
+    let mut polymer_pairs: HashMap<(char, char), usize> = Default::default();
+    for pair in original.chars().tuple_windows() {
+        *polymer_pairs.entry(pair).or_default() += 1;
+    }
+    *polymer_pairs.entry((' ', original.chars().next().unwrap())).or_default() += 1;
+    *polymer_pairs.entry((original.chars().next_back().unwrap(), ' ')).or_default() += 1;
+
+    for _ in 0..iterations {
+        polymer_pairs = expand_polymer(&polymer_pairs, &rules);
+    }
+
+    let mut counts: HashMap<char, usize> = HashMap::new();
+    for ((a, b), count) in polymer_pairs {
+        *counts.entry(a).or_default() += count;
+        *counts.entry(b).or_default() += count;
+    }
+    counts.remove(&' ').unwrap();
+
+    let max_count = counts.values().max().unwrap() / 2;
+    let min_count = counts.values().min().unwrap() / 2;
+
+    max_count - min_count
+}
+
+fn solve_part1(original: &str, rules: &[(char, char, char)]) -> usize {
+    solve_iterations(original, rules, 10)
+}
+
+fn solve_part2(original: &str, rules: &[(char, char, char)]) -> usize {
+    solve_iterations(original, rules, 40)
 }
