0d9cff58679382ff5411d7c1f338ed36a4adf7c6
Predrag Gruevski <obi1kenobi82@gmail.com>
2021-12-28 21:38:33 -0500

    Finish mul_input_range_analysis with copious tests.

--- day24/src/analysis/range_analysis.rs
+++ day24/src/analysis/range_analysis.rs
@@ -9,10 +9,65 @@ pub(super) fn mul_input_range_analysis(
     let (operand_negative, operand_zero, operand_positive) = split_range(&operand);
     let (result_negative, result_zero, result_positive) = split_range(&result);
 
-    // let mut source_possibilities = vec![];
-    // let mut operand_possibilities = vec![];
+    let mut source_possibilities = vec![];
+    let mut operand_possibilities = vec![];
+    let mut result_possibilities = vec![];
+    let mut add_to_vecs = |maybe_ranges: Option<(ValueRange, ValueRange, ValueRange)>| {
+        if let Some((src, op, res)) = maybe_ranges {
+            source_possibilities.push(src);
+            operand_possibilities.push(op);
+            result_possibilities.push(res);
+        }
+    };
 
-    todo!()
+    let same_sign_tuples = [
+        (source_negative.clone(), operand_negative.clone(), result_positive.clone()),
+        (source_positive.clone(), operand_positive.clone(), result_positive),
+    ];
+    for tuple in same_sign_tuples {
+        if let (Some(source), Some(operand), Some(result)) = tuple {
+            add_to_vecs(mul_input_range_same_sign_input_ranges(source, operand, result));
+        }
+    }
+
+    let opposite_sign_tuples = [
+        (source_negative.clone(), operand_positive.clone(), result_negative.clone()),
+        (source_positive.clone(), operand_negative.clone(), result_negative),
+    ];
+    for tuple in opposite_sign_tuples {
+        if let (Some(source), Some(operand), Some(result)) = tuple {
+            add_to_vecs(mul_input_range_opposite_sign_input_ranges(source, operand, result));
+        }
+    }
+
+    let zero_tuples = [
+        (source_positive, operand_zero.clone(), result_zero.clone()),
+        (source_negative, operand_zero.clone(), result_zero.clone()),
+        (source_zero.clone(), operand_positive, result_zero.clone()),
+        (source_zero.clone(), operand_negative, result_zero.clone()),
+        (source_zero, operand_zero, result_zero),
+    ];
+    for tuple in zero_tuples {
+        // If all three ranges are possible, then since multiplying anything by zero gives zero,
+        // the entire ranges are possible.
+        if let (Some(source), Some(operand), Some(result)) = tuple {
+            add_to_vecs(Some((source, operand, result)));
+        }
+    }
+
+    let final_source = ValueRange::new(
+        source_possibilities.iter().map(|r| r.start()).min().unwrap(),
+        source_possibilities.iter().map(|r| r.end()).max().unwrap(),
+    );
+    let final_operand = ValueRange::new(
+        operand_possibilities.iter().map(|r| r.start()).min().unwrap(),
+        operand_possibilities.iter().map(|r| r.end()).max().unwrap(),
+    );
+    let final_result = ValueRange::new(
+        result_possibilities.iter().map(|r| r.start()).min().unwrap(),
+        result_possibilities.iter().map(|r| r.end()).max().unwrap(),
+    );
+    (final_source, final_operand, final_result)
 }
 
 // Solve "left * right = result" for left,
@@ -336,6 +391,134 @@ mod tests {
         assert_eq!(
             (expected_source, expected_operand, expected_result),
             mul_input_range_opposite_sign_input_ranges(source, operand, result).unwrap()
+        );
+    }
+
+    #[test]
+    fn test_mul_input_range_exact_zero_result_source_must_be_zero() {
+        let source = ValueRange::new(-70, 100);
+        let operand = ValueRange::new(2, 25);
+        let result = ValueRange::new_exact(0);
+
+        let expected_source = ValueRange::new_exact(0);
+        let expected_operand = ValueRange::new(2, 25);
+        let expected_result = ValueRange::new_exact(0);
+
+        assert_eq!(
+            (expected_source, expected_operand, expected_result),
+            mul_input_range_analysis(source, operand, result)
+        );
+    }
+
+    #[test]
+    fn test_mul_input_range_exact_zero_result_operand_must_be_zero() {
+        let source = ValueRange::new(7, 10);
+        let operand = ValueRange::new(-2, 25);
+        let result = ValueRange::new_exact(0);
+
+        let expected_source = ValueRange::new(7, 10);
+        let expected_operand = ValueRange::new_exact(0);
+        let expected_result = ValueRange::new_exact(0);
+
+        assert_eq!(
+            (expected_source, expected_operand, expected_result),
+            mul_input_range_analysis(source, operand, result)
+        );
+    }
+
+    #[test]
+    fn test_mul_input_range_exact_zero_result_either_can_be_zero() {
+        let source = ValueRange::new(-3, 11);
+        let operand = ValueRange::new(-7, 8);
+        let result = ValueRange::new_exact(0);
+
+        let expected_source = ValueRange::new(-3, 11);
+        let expected_operand = ValueRange::new(-7, 8);
+        let expected_result = ValueRange::new_exact(0);
+
+        assert_eq!(
+            (expected_source, expected_operand, expected_result),
+            mul_input_range_analysis(source, operand, result)
+        );
+    }
+
+    #[test]
+    fn test_mul_input_range_inputs_all_span_zero() {
+        let source = ValueRange::new(-3, 11);
+        let operand = ValueRange::new(-7, 8);
+        let result = ValueRange::new(-1, 1);
+
+        let expected_source = ValueRange::new(-3, 11);
+        let expected_operand = ValueRange::new(-7, 8);
+        let expected_result = ValueRange::new(-1, 1);
+
+        assert_eq!(
+            (expected_source, expected_operand, expected_result),
+            mul_input_range_analysis(source, operand, result)
+        );
+    }
+
+    #[test]
+    fn test_mul_input_range_inputs_span_zero_output_is_positive() {
+        let source = ValueRange::new(-3, 11);
+        let operand = ValueRange::new(-7, 8);
+        let result = ValueRange::new(3, 16);
+
+        let expected_source = ValueRange::new(-3, 11);
+        let expected_operand = ValueRange::new(-7, 8);
+        let expected_result = ValueRange::new(3, 16);
+
+        assert_eq!(
+            (expected_source, expected_operand, expected_result),
+            mul_input_range_analysis(source, operand, result)
+        );
+    }
+
+    #[test]
+    fn test_mul_input_range_inputs_span_zero_output_is_positive2() {
+        let source = ValueRange::new(-3, 11);
+        let operand = ValueRange::new(-23, 8);
+        let result = ValueRange::new(3, 7);
+
+        let expected_source = ValueRange::new(-3, 7);
+        let expected_operand = ValueRange::new(-7, 7);
+        let expected_result = ValueRange::new(3, 7);
+
+        assert_eq!(
+            (expected_source, expected_operand, expected_result),
+            mul_input_range_analysis(source, operand, result)
+        );
+    }
+
+    #[test]
+    fn test_mul_input_range_inputs_span_zero_output_is_negative() {
+        let source = ValueRange::new(-11, 3);
+        let operand = ValueRange::new(-8, 7);
+        let result = ValueRange::new(-16, -3);
+
+        let expected_source = ValueRange::new(-11, 3);
+        let expected_operand = ValueRange::new(-8, 7);
+        let expected_result = ValueRange::new(-16, -3);
+
+        assert_eq!(
+            (expected_source, expected_operand, expected_result),
+            mul_input_range_analysis(source, operand, result)
+        );
+    }
+
+    #[test]
+    fn test_mul_input_range_inputs_span_zero_output_is_negative2() {
+        let source = ValueRange::new(-11, 3);
+        let operand = ValueRange::new(-8, 23);
+        let result = ValueRange::new(-7, -3);
+
+        let expected_source = ValueRange::new(-7, 3);
+        let expected_operand = ValueRange::new(-7, 7);
+        let expected_result = ValueRange::new(-7, -3);
+
+        assert_eq!(
+            (expected_source, expected_operand, expected_result),
+            mul_input_range_analysis(source, operand, result)
         );
     }
 }
